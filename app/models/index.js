97e52352300bb855863a30488bb42d0609fb4cd7
Eduardo Denis <edigde@gmail.com>
2019-06-25 11:02:31 -0300

    fix sequelize conenction on heroku

--- app/models/index.js
+++ app/models/index.js
@@ -13,9 +13,9 @@ if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
-    logging:  false //true
+    dialectOptions: {
+        ssl: true
+    }
   })
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config)
