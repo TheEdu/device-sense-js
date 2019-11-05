63731cfb694c80a42394f0c8cad909022d1b7e8a
Eduardo Denis <edigde@gmail.com>
2019-11-05 20:23:44 +0000

    fix datatable

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -1,34 +1,46 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
 
-  $('#dataTable').DataTable({
-    "language": {
-      "lengthMenu": "Mostrar _MENU_ registros por página",
-      "zeroRecords": "No hay datos para mostrar",
-      "info": "Página _PAGE_ de _PAGES_",
-      "infoEmpty": "No hay registros disponibles",
-      "infoFiltered": "(filtrado de _MAX_ registros)",
-      "search": "Buscar:",
-      "paginate": {
+  // Setup - add a text input to each footer cell
+  $('#dataTable thead tr').clone(true).appendTo( '#dataTable thead' );
+  $('#dataTable thead tr:eq(1) th').each( function (i) {
+      var title = $(this).text();
+      if (title != "Acciones") {
+        $(this).html( '<input type="text" class="form-control filter-column-table" placeholder="Buscar '+title+'" />' );
+        $(this).css('padding', '5px');
+        $( 'input', this ).on( 'keyup change', function () {
+          console.log("input: " + this.value)
+          console.log(i, table_main.column(i))
+            if ( table_main.column(i).search() !== this.value ) {
+                table_main
+                    .column(i)
+                    .search( this.value )
+                    .draw();
+            }
+        } );
+      } else {
+        $(this).html("");
+      }
+  } );
+
+  const table_main = $('#dataTable').DataTable({
+    bSort: true,
+    bPaginate: true,
+    bFilter: true,
+    orderCellsTop: true,
+    fixedHeader: true,
+    lengthMenu: [ 5, 7, 10 ],
+    language: {
+      lengthMenu: "Mostrar _MENU_ registros por página",
+      zeroRecords: "No hay datos para mostrar",
+      info: "Página _PAGE_ de _PAGES_",
+      infoEmpty: "No hay registros disponibles",
+      infoFiltered: "(filtrado de _MAX_ registros)",
+      search: "Buscar:",
+      paginate: {
         "previous": "Anterior",
         "next": "Siguiente"
       }
-    }
-  });
-
-  $('#dataTable-suscription').DataTable({
-    "lengthMenu": [ 5, 7, 10 ],
-    "language": {
-      "lengthMenu": "Mostrar _MENU_ registros por página",
-      "zeroRecords": "No hay datos para mostrar",
-      "info": "Página _PAGE_ de _PAGES_",
-      "infoEmpty": "No hay registros disponibles",
-      "infoFiltered": "(filtrado de _MAX_ registros)",
-      "search": "Buscar:",
-      "paginate": {
-        "previous": "Anterior",
-        "next": "Siguiente"
-      },
     }
   });
 
